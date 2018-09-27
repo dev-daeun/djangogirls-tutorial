5fbb82d8acd565d5ddae3e2711e2311bf6c74ebf
DaEun <kde6260@gmail.com>
2018-09-27 16:55:50 +0900

    뷰 생성 및 url 설정

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
 
-# Create your views here.
+def post_list(request):
+    """
+
+    :param request: HTTP 요청 객체
+    :return:
+    """
+    # 리전에 맞는 시간 객체 할당
+    current_time = timezone.now()
+    return HttpResponse(
+        '<html>'
+        '<body>'
+        '<h1>Post List</h1>'
+        '<p>{}<p>'
+        '</body>'
+        '</html>'.format(current_time.strftime('%Y. %m. %d. %H:%M'))
+    )
